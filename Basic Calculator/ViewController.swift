7c79f32d35b012a8ed957f0c2369de3189117c0d
furkankarakocmobilist <furkan.karakoc@mobilistbilisim.com>
2023-01-10 17:09:41 +0300

    Initial Commit

---
+++ Basic Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic Calculator
+//
+//  Created by Furkan on 10.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
