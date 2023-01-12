de8b5caa18b7776b35c03934553d9cb0919ec4d5
furkankarakocmobilist <furkan.karakoc@mobilistbilisim.com>
2023-01-13 00:06:46 +0300

    Make a basic calculator for beginners

--- Basic Calculator/ViewController.swift
+++ Basic Calculator/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var result : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func sumClicked(_ sender: Any) {
+        
+        if let firstnumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                result = firstnumber + secondNumber
+                resultLabel.text = "Result: \(String(result))"
+            }
+        }
+    }
+    
+    @IBAction func minusClicked(_ sender: Any) {
+        if let firstnumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                result = firstnumber - secondNumber
+                resultLabel.text = "Result: \(String(result))"
+            }
+        }
+    }
+    
+    @IBAction func multiplyClicked(_ sender: Any) {
+        if let firstnumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                result = firstnumber * secondNumber
+                resultLabel.text = "Result: \(String(result))"
+            }
+        }
+    }
+    
+    @IBAction func divideClicked(_ sender: Any) {
+        if let firstnumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                result = firstnumber / secondNumber
+                resultLabel.text = "Result: \(String(result))"
+            }
+        }
+    }
+    
+    @IBAction func clearClicked(_ sender: Any) {
+        resultLabel.text = "Result: 3"
+    }
 }
 
